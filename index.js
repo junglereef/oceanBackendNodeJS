af8b450de1253c8642963ac607908b3c988b7c69
junglereef <atila.iglesias@hotmail.com>
2022-06-20 12:01:46 -0300

    endpoints

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+const herois = ["Mulher Maeavilha", "Capitão Marvel"];
+
+app.get("/herois", function(req, res) {
+  res.send(herois);
+})
+
 app.listen(3000, () =>
   console.log("Servidor rodando em http://localhost:3000")
 );
