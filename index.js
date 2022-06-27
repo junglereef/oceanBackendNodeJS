142441bae5d8d1fe46bedaa4b77fc6df2c770ea3
junglereef <atila.iglesias@hotmail.com>
2022-06-27 12:42:16 -0300

    Connection to database

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const express = require("express");
+//MongoClient, ObjectID
+const { MongoClient, ObjectId } = require("mongodb");
+
+const url = "mongodb://localhost:27017";
+const dbName = "ocean_bancodedados_07_08_2022"
+
+async function main() {
+console.log("Conectando ao Banco de dados...")
+
+//conexão com o banco de dados
+const client = await MongoClient.connect(url);
+
+const db = client.db(dbName);
+
+
+const collection = db.collection("herois")
+
+console.log("Conexão realizada com sucesso ");
+
+//Aplicação Backend com Express 
+
 const app = express();
+
 
 // Informar para o Express que estamos usando JSON
 // no body das requisições
@@ -12,29 +34,34 @@ app.get("/", function (req, res) {
 const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
 
 // Endpoint Read All - [GET] /herois
-app.get("/herois", function (req, res) {
-  
-  res.send(herois.filter(Boolean));
+app.get("/herois", async function (req, res) {
+  const documentos = await collection.find().toArray()
+
+  res.send(documentos);
 });
 
 // Endpoint Read by ID - [GET] /herois/:id
-app.get("/herois/:id", function (req, res) {
+app.get("/herois/:id", async function (req, res) {
   const id = req.params.id;
 
-  const item = herois[id - 1];
+  const item = await collection.findOne({ _id: new ObjectId(id)});
 
   res.send(item);
 });
 
 // Endpoint Create - [POST] /herois
-app.post("/herois", function (req, res) {
+app.post("/herois", async function (req, res) {
   // Acessa o nome do herói no corpo da requisição
-  const item = req.body.nome;
+  const item = req.body;
+
 
 // Insere o nome na lista
-  herois.push(item);
+  // herois.push(item);
+  /** */
+  //insere o objeto na collection
+  await collection.insertOne(item)
 
-  res.send("Item inserido com sucesso!");
+  res.send(item);
 });
 
 //Endpoint Update - [PUT] /herois/:id
@@ -43,22 +70,36 @@ app.put("/herois/:id" , function(req,res){
   const id = req.params.id;
   
   //Pegamos o nome que foi enviado no corpo
-  const item = req.body.nome;
+  const item = req.body;
 
   //Atualizamos o ID para iniciar do 1
-  herois[id - 1] = item
-  res.send("atualizar um item.")
+  // herois[id - 1] = item
+  //atualizamos a collection usando o IDm colocando o novo item
+  collection.updateOne(
+    //filtro
+    { _id: new ObjectId(id)},
+    //operação de atualização 
+    { $set: item }
+  );
+
+  res.send(item)
 });
 
 //Endpoint Delete - [DELETE] /herois/:id
-app.delete("/herois/:id", function (req,res) {
+app.delete("/herois/:id", async function (req,res) {
   //obtemos o ID pela rota
   const id = req.params.id;
   //removemos a posição id - da lista
-  delete herois [id - 1]
+  // delete herois [id - 1]
+
+  //removemos o documento pelo id 
+  await collection.deleteOne({ _id: new ObjectId(id)})
   res.send("Item Removido.");
 } )
 
 app.listen(3000, () =>
   console.log("Servidor rodando em http://localhost:3000")
 );
+};
+
+main();
